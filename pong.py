4ea15b643218d9eab39e90e3e3dd9c64f74bd9ed
Alias M6 <aliasmontoya719@gmail.com>
2019-11-14 05:30:59 -0700

    Setup turtle window object specifications and main game loop

---
+++ pong.py
@@ -0,0 +1,11 @@
+import turtle
+
+wn = turtle.Screen();
+wn.title("Pong by @TokyoEdTech")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main Game Loop
+while True:
+    wn.update() 
