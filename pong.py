42d09596e4d20213473b3d39689b710ae65b148e
Alias M6 <aliasmontoya719@gmail.com>
2019-11-15 06:27:40 -0700

    create functions that allow for movement of paddle b

--- pong.py
+++ pong.py
@@ -48,10 +48,22 @@ def paddle_a_down():
     y -= 20
     paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 #Keyboard Binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main Game Loop
 while True:
